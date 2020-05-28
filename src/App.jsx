bf69d1b5e846f25c18933b31fd71505f444ab4a7
nanori <leonor.ferreira@gmail.com>
2020-05-28 10:52:52 +0100

    Iteration 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import './App.scss';
+import meals from './meals.json';
+// import meals from './meals';
+// import Button from 'react-bootstrap/Button';
+// import Image from 'react-bootstrap/Image';
+// import InputGroup from 'react-bootstrap/InputGroup';
+// import Form from 'react-bootstrap/Form';
+// import Container from 'react-bootstrap/Container'
+// import Media from 'react-bootstrap/Media'
 
-import meals from './meals';
+const MealBox = (props) => {
+  return (
+    <div>
+      <img className="img-small" src={props.image} alt={props.image} />
+      <h5> {props.name} </h5>
+      <small>{props.calories}</small>
+    </div>
+  );
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      meals: meals,
+      firstMeal: meals.slice(0, 1)
+    };
+    console.log('meals', meals);
+  }
+
   render() {
     return (
       <div>
-        <p>Sample App</p>
+        {this.state.firstMeal.map((meal) => {
+          return <MealBox image={meal.image} name={meal.name} calories={meal.calories} />;
+        })}
       </div>
     );
   }
